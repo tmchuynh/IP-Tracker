d7c369d299a8d8bf1797efe79a803ae519c13430
Tina Huynh <tinamchuynh@gmail.com>
2023-02-23 19:31:16 -0800

    upload working information

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,35 @@
 const _ = "fb546139f933b8664332f96848b641153631454";
 
+var address = document.querySelector('.address');
+var locations = document.querySelector('.location');
+var timezone = document.querySelector('.timezone');
+var isp = document.querySelector('.isp');
+
+
 if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position.coords.latitude);
-        console.log(position.coords.longitude);
-    })
+    $.get("https://api64.ipify.org?format=json", function (data) {
+        console.log(data.ip);
+        getIP(data.ip);
+    });
+}
+
+function getInput(e) {
+    getIP(e.value);
+}
+
+function getIP(e) {
+    $.get("http://ip-api.com/json/" + e, function (data) {
+        console.log(data);
+        address.innerHTML = e;
+        timezone.innerHTML = data.timezone;
+
+        $.get("https://api.geocod.io/v1.7/reverse?q=" + data.lat + "," + data.lon + "&api_key=" + _, function (response) {
+            locations.innerHTML = response.results[0].address_components.city + ", " + response.results[0].address_components.country;
+            console.log(response.results[0]);
+        });
+
+        isp.innerHTML = data.isp;
+
+    });
+
 }
